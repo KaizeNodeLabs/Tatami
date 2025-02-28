8e258e1552b2006d09796715d19c1fc5c6065351
Sebastian Gomez <sebastianga48l@gmail.com>
2025-02-27 19:29:05 -0600

    feat: navbar add modes

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,9 +1,9 @@
-import Productive from "@/components/productive/productive";
-import WhyChooseTatami from "./why-choose-us/why-choose-us";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import PartnersSection from "@/components/AlignedWith/AligendWith";
 import JoinCommunity from "@/components/JoinTheCommunity/JoinTheCommunity";
+import Productive from "@/components/productive/productive";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import WhyChooseTatami from "./why-choose-us/why-choose-us";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
@@ -14,7 +14,6 @@ export default function Home() {
       <Link href="/app">
         <Button>Go to App</Button>
       </Link>
-
     </div>
   );
 }
